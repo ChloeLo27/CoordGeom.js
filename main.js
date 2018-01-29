20b0b996f6f528381e970b889eaf2242e80ee212
chloeCP <31690283+chloeCP@users.noreply.github.com>
2018-01-29 16:17:37 +0800

    Update main.js
    added annotation for type of input and output for functions

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // - MARK: defining properties of class Point
 class Point {
   // init function
-  constructor(x,y) {
+  constructor(x,y) { // TYPE: float, float
     this.x = x; // TYPE: float
     this.y = y; // TYPE: float
   }
@@ -18,18 +18,18 @@ class Point {
   }
   
   // methods
-  translate(x, y) {
+  translate(x, y) { // TYPE: float, float
     this.x = this.x + x;
     this.y = this.y + y;
     return this; // TYPE: Point
   }
-  translateByVector(vector) {
+  translateByVector(vector) { // TYPE: Vector
     this.x = this.x + vector.x;
     this.y = this.y + vector.y;
     return this;
   }
   // all below methods are relative to origin or the axes
-  rotate(angle) {
+  rotate(angle) { // TYPE: float
     var x = this.x;
     var y = this.y;
     this.x = x*Math.cos(angle) - y*Math.sin(angle);
@@ -44,7 +44,7 @@ class Point {
     this.y = -this.y;
     return this; // TYPE: Point
   }
-  update(x,y) {
+  update(x,y) { // TYPE: float, float
     this.x = x;
     this.y = y;
     return this; // TYPE: Point
@@ -57,7 +57,7 @@ class Point {
 // - MARK: define class Line
 class Line {
   // init
-  constructor(point1, point2) {
+  constructor(point1, point2) { // TYPE: Point, Point
     this.point1 = point1.clone();
     this.point2 = point2.clone();
   }
@@ -103,14 +103,14 @@ class Line {
   }
   
   // methods
-  calY(x) {
+  calY(x) { // TYPE: float
     if (!isNaN(this.m)) {
       return this.m*x + this.c; // TYPE: float
     } else {
       return NaN;
     }
   }
-  calX(y) {
+  calX(y) { // TYPE: float
     if (!isNaN(this.m)) {
       if (this.m != 0) {
         return (y - this.c)/this.m; // TYPE: float
@@ -126,7 +126,7 @@ class Line {
 // - MARK: define class lineSegment
 class LineSegment {
   // init
-  constructor(point1, point2) {
+  constructor(point1, point2) { // TYPE: Point, Point
     this.point1 = point1.clone(); // TYPE: Point
     this.point2 = point2.clone(); // TYPE: Point
   }
@@ -159,7 +159,7 @@ class LineSegment {
 class Vector {
   
   // init properties
-  constructor(x, y) {
+  constructor(x, y) { // TYPE: float, float
     this.x = x; // TYPE: float
     this.y = y; // TYPE: float
   }
@@ -195,7 +195,7 @@ class Vector {
   }
   
   // methods
-  rotate(angle) {
+  rotate(angle) { // TYPE: float
     var x = this.x;
     var y = this.y;
     this.x = x*Math.cos(angle) - y*Math.sin(angle);
@@ -214,16 +214,16 @@ class Vector {
     this.flipX().flipY();
     return this; // TYPE: Vector
   }
-  scale(factor) {
+  scale(factor) { // TYPE: float
     this.x = this.x*factor;
     this.y = this.y*factor;
     return this; // TYPE: Vector
   }
-  scaleX(factor) {
+  scaleX(factor) { // TYPE: float
     this.x = this.x*factor;
     return this; // TYPE: Vector
   }
-  scaleY(factor) {
+  scaleY(factor) { // TYPE: float
     this.y = this.y*factor;
     return this; // TYPE: Vector
   }
@@ -233,7 +233,7 @@ class Vector {
 class Polygon {
   
   // init
-  constructor(points) {
+  constructor(points) { // TYPE: [Point]
     var pointsClone = [];
     var edges = [];
     for (var i=0; i<points.length; i++) {
@@ -276,7 +276,7 @@ class Polygon {
   }
   
   // methods
-  translate(x,y) {
+  translate(x,y) { // TYPE: float, float
     for (var i=0; i<this.numberOfVertices; i++) {
       this.vertices[i].translate(x,y);
     }
@@ -289,7 +289,7 @@ class Polygon {
 class Circle {
   
   // init
-  constructor(center, radius) {
+  constructor(center, radius) { // TYPE: Point, float
     this.center = center.clone(); // TYPE: Point
     this.radius = radius; // TYPE: float
   }
@@ -306,11 +306,11 @@ class Circle {
   }
   
   // methods
-  translate(x,y) {
+  translate(x,y) { // TYPE: float, float
     this.center.translate(x,y);
     return this; // TYPE: Circle
   }
-  setRadius(r) {
+  setRadius(r) { // TYPE: float
     this.radius = r;
     return this; // TYPE: Circle
   }
@@ -318,7 +318,7 @@ class Circle {
 
 
 // MARK: - geometrical functions
-function interceptOfLines(line1, line2) {
+function interceptOfLines(line1, line2) { // TYPE: Line, Line
   if ((line1.m != line2.m) && !isNaN(line1.m) && !isNaN(line2.m)) {
     var x = (line2.c-line1.c)/(line1.m-line2.m);
     return new Point(x,line1.m*x+line1.c); // TYPE: Point
@@ -334,36 +334,36 @@ function interceptOfLines(line1, line2) {
     }
   }
 }
-function lineFromPointSlope(point, m) {
+function lineFromPointSlope(point, m) { // TYPE: Point, float
   var point2 = newPointTranslatedByVector(point, new Vector(1,m));
   return new Line(point, point2); // TYPE: Line
 }
-function lineFromLineSegment(lineSegment) {
-  return new Line(lineSegment.point1, lineSegment.point2);
+function lineFromLineSegment(lineSegment) { // TYPE: LineSegment
+  return new Line(lineSegment.point1, lineSegment.point2); // TYPE: Line
 }
-function newPointTranslatedByVector(point, vector) {
+function newPointTranslatedByVector(point, vector) { // TYPE: Point, Vector
   return new Point(point.x+vector.x, point.y+vector.y); // TYPE: Point
 }
-function newPointReflectedInLine(point, line) {
+function newPointReflectedInLine(point, line) { // TYPE: Point, Line
   if (!isNaN(line.m) && (line.m != 0)) {
     var m2 = -1/line.m;
     var line2 = lineFromPointSlope(point, m2);
     var intercept = interceptOfLines(line, line2);
     var change = vectorFromPoints(point, intercept);
-    return newPointTranslatedByVector(intercept, change);
+    return newPointTranslatedByVector(intercept, change); // TYPE: Point
   } else if (line.m == 0) {
-    return new Point(point.x, 2*line.c - point.y);
+    return new Point(point.x, 2*line.c - point.y); // TYPE: Point
   } else {
     return new Point(2*line.xIntercept.x - point.x, point.y); // TYPE: Point
   }
 }
-function vectorFromPoints(point1, point2) {
+function vectorFromPoints(point1, point2) { // TYPE: Point, Point
   return new Vector(point2.x - point1.x, point2.y - point1.y); // TYPE: Vector
 }
-function dotProduct(vector1, vector2) {
+function dotProduct(vector1, vector2) { // TYPE: Vector, Vector
   return vector1.x*vector2.x + vector1.y*vector2.y; // TYPE: float
 }
-function angleBetweenVectors(vector1, vector2) {
+function angleBetweenVectors(vector1, vector2) { // TYPE: Vector, Vector
   if (!vector1.isZeroVector && !vector2.isZeroVector) {
     return Math.acos(dotProduct(vector1, vector2)/(vector1.magnitude*vector2.magnitude)); // TYPE: float
   } else {
@@ -371,12 +371,12 @@ function angleBetweenVectors(vector1, vector2) {
   }
 }
 function addVectors(vector1, vector2) { // TYPE: Vector, Vector
-  return new Vector(vector1.x+vector2.x, vector1.y+vector2.y);
+  return new Vector(vector1.x+vector2.x, vector1.y+vector2.y); // TYPE: Vector
 }
 function subtractVectors(vector1, vector2) { // TYPE: Vector, Vector
-  return new Vector(vector1.x-vector2.x, vector1.y-vector2.y);
+  return new Vector(vector1.x-vector2.x, vector1.y-vector2.y); // TYPE: Vector
 }
-function intersectionOfCircleAndLine(circle, line) {
+function intersectionOfCircleAndLine(circle, line) { // TYPE: Circle, Line
   var r = circle.radius;
   var a = circle.center.x;
   var b = circle.center.y;
@@ -410,7 +410,7 @@ function intersectionOfCircleAndLine(circle, line) {
     }
   }
 }
-function intersectionOfCircles(circle1, circle2) {
+function intersectionOfCircles(circle1, circle2) { // TYPE: Circle, Circle
   // check whether the two circles overlapped
   var center1ToCenter2 = vectorFromPoints(circle1.center, circle2.center);
   var d = center1ToCenter2.magnitude;
@@ -434,10 +434,10 @@ function intersectionOfCircles(circle1, circle2) {
     return []; // TYPE: [Point]
   }
 }
-function pointIsOnLine(point, line) {
+function pointIsOnLine(point, line) { // TYPE: Point, Line
   return (point.y == line.m*point.x + line.c); // TYPE: bool
 }
-function pointIsOnLineSegment(point, lineSegment) {
+function pointIsOnLineSegment(point, lineSegment) { // TYPE: Point, LineSegment
   var point1 = lineSegment.point1;
   var point2 = lineSegment.point2;
   var line = new Line(point1, point2);
@@ -445,19 +445,19 @@ function pointIsOnLineSegment(point, lineSegment) {
   var upperBound = Math.max(point1.x, point2.x);
   return (pointIsOnLine(point, line) && (point.x >= lowerBound) && (point.x <= upperBound)); // TYPE: bool
 }
-function pointIsStrictlyInCircle(point, circle) {
+function pointIsStrictlyInCircle(point, circle) { // TYPE: Point, Circle
   var pointFromCenter = vectorFromPoints(point, circle.center);
-  return (pointFromCenter.magnitude < circle.radius);
+  return (pointFromCenter.magnitude < circle.radius); // TYPE: bool
 }
-function pointIsOnCircleCircumference(point, circle) {
+function pointIsOnCircleCircumference(point, circle) { // TYPE: Point, Circle
   var pointFromCenter = vectorFromPoints(point, circle.center);
-  return (pointFromCenter.magnitude == circle.radius);
+  return (pointFromCenter.magnitude == circle.radius); // TYPE: bool
 }
-function pointIsStrictlyOutOfCircle(point, circle) {
+function pointIsStrictlyOutOfCircle(point, circle) { // TYPE: Point, Circle
   var pointFromCenter = vectorFromPoints(point, circle.center);
-  return (pointFromCenter.magnitude > circle.radius);
+  return (pointFromCenter.magnitude > circle.radius); // TYPE: bool
 }
-function pointIsOnPolygonEdge(point, polygon) {
+function pointIsOnPolygonEdge(point, polygon) { // TYPE: Point, Polygon
   var onEdge = false;
   polygon.edges.forEach(function(edge) {
     if (pointIsOnLineSegment(point, edge)) {
@@ -466,7 +466,7 @@ function pointIsOnPolygonEdge(point, polygon) {
   });
   return onEdge; // TYPE: bool
 }
-function pointIsStrictlyInPolygon(point, polygon) {
+function pointIsStrictlyInPolygon(point, polygon) { // TYPE: Point, Polygon
   var windingNumber = 0;
   polygon.edges.forEach(function(edge) {
     var thisVertex = edge.point1;
@@ -488,10 +488,10 @@ function pointIsStrictlyInPolygon(point, polygon) {
   });
   return (Math.abs(windingNumber) == 1); // TYPE: bool
 }
-function pointIsStrictlyOutOfPolygon(point, polygon) {
+function pointIsStrictlyOutOfPolygon(point, polygon) { // TYPE: Point, Polygon
   return (!pointIsOnPolygonEdge(point, polygon) && !pointIsStrictlyInPolygon(point, polygon)); // TYPE: bool
 }
-function pointsDrawPolygon(points) {
+function pointsDrawPolygon(points) { // TYPE: [Point]
   if (points.length < 3) {
     return false;
   } else {
@@ -518,18 +518,18 @@ function pointsDrawPolygon(points) {
 
 // MARK: - helper functions
 
-function lineIsLeftOfPoint(line, point) {
+function lineIsLeftOfPoint(line, point) { // TYPE: Line, Point
   if (!isNaN(line.m)) {
     if (line.m == 0) {
-      return false;
+      return false; // TYPE: bool
     } else {
       if (pointIsOnLine(point, line)) {
-        return false
+        return false; // TYPE: bool
       } else {
-        return (line.calX(point.y) < point.x);
+        return (line.calX(point.y) < point.x); // TYPE: bool
       }
     }
   } else {
-    return (line.xIntercept.x < point.x);
+    return (line.xIntercept.x < point.x); // TYPE: bool
   }
 }
